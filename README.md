851de551e9a4fc74bd61c46cef4bb5a6a21aafa3
Tirso Lecointere <tirsolecointere@gmail.com>
2021-06-23 16:02:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# "gulp-file-include" Starter Template
+
+## Quick start
+
+* Navigate to project folder after cloning.
+* Run `npm install`.
+* Run `gulp` to start file-include compiler, live server, sass compiler, etc.
+
+Files are compiled into `/build` directory.
---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+const { includes } = require('lodash');
+
+var gulp                = require('gulp'),
+    browserSync         = require('browser-sync').create(),
+    sass                = require('gulp-sass'),
+    // uglify              = require('gulp-uglify'),
+    autoprefixer        = require('gulp-autoprefixer'),
+    cleanCSS            = require('gulp-clean-css'),
+    // imagemin            = require('gulp-imagemin'),
+    fileinclude         = require('gulp-file-include');
+    // htmlmin             = require('gulp-htmlmin'),
+    concat              = require('gulp-concat');
+
+// Static Server + watching scss/js/html files.
+gulp.task('serve', ['sass', 'compressJs', 'fileinclude-watch', 'compressImage'], function () {
+    browserSync.init({
+        server: './build'
+    });
+
+    gulp.watch('src/assets/sass/*.sass', ['sass']);
+    gulp.watch('src/assets/js/*.js', ['compressJs']);
+    gulp.watch('src/assets/img/*', ['compressImage']);
+    gulp.watch('./**/*.html', ['fileinclude-watch'])
+});
+
+// Sass task.
+gulp.task('sass', function () {
+    return gulp.src('src/assets/sass/*.sass')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./build/assets/css'))
+        .pipe(browserSync.stream());
+});
+
+// Js task.
+gulp.task('compressJs', function () {
+    return gulp.src('src/assets/js/*.js')
+        // .pipe(uglify())
+        .pipe(gulp.dest('./build/assets/js'))
+});
+
+// Images task.
+gulp.task('compressImage', function () {
+    return gulp.src('src/assets/img/**')
+        // .pipe(imagemin({
+        //     progressive: true,
+        //     optimizationLevel: 3
+        // }))
+        .pipe(gulp.dest('./build/assets/img'))
+});
+
+// gulp include task
+gulp.task('fileinclude', function() {
+    gulp.src('src/pages/*.html')
+    .pipe(fileinclude({
+        prefix: '@@',
+        basepath: 'src/includes'
+    }))
+    .pipe(gulp.dest('./build'));
+});
+
+// Create a task that ensures the `fileinclude` task is complete before reloading browsers.
+gulp.task('fileinclude-watch', ['fileinclude'], function () {
+    browserSync.reload();
+});
+
+// Vendor scripts tasks.
+gulp.task('vendors-scripts', function () {
+    return gulp.src([
+            './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+        ])
+        .pipe(concat('vendors.js'))
+        .pipe(gulp.dest('./build/assets/js/'));
+});
+
+// Copyfiles task.
+gulp.task('copy-files', function () {
+    gulp.src([
+        'src/config/web.config'
+    ])
+    .pipe(gulp.dest('./build'));
+});
+
+// Compile project.
+gulp.task('build-project', [
+    'sass',
+    'compressImage',
+    'compressJs',
+    'fileinclude',
+    'vendors-scripts',
+    'copy-files']
+);
+
+// Compile and start project.
+gulp.task('default', ['build-project', 'serve']);
